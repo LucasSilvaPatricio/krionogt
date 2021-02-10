5c9816855ffb8b523aa6c6c8d924c6c5a5d073c1
Mirelaoliveira <43251181+Mirelaoliveira@users.noreply.github.com>
2021-02-10 11:52:33 -0300

    instalador python, nmap, figlet, toilet

---
+++ python_install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#############################
+#    creditos [ghost]
+#############################
+
+echo "Instalador"
+echo "Serão instalado os seguintes programas"
+echo "[Python, Figlet, Toilet, Nmap]"
+
+echo "Instalando python -> pkg install python"
+sleep 1
+
+# comando para instalar o python
+pkg install python -y
+
+echo "Instalando figlet"
+sleep 1
+
+# comando para instalar figlet
+pkg install figlet -y
+
+echo "Instalando toilet"
+sleep 1
+
+# comando para instalar toilet
+pkg install toilet -y
+
+echo "Instalando nmap"
+sleep 1
+
+# comando para instalar nmap
+pkg install nmap -y
+
+sleep 1
+
+echo -e "\033[32m"
+toilet "Instalação concluida"
+echo -e "\033[0;34mScript criado por [GHOST]"
